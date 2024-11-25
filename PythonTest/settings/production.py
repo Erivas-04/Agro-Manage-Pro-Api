663c054bb594d9b29c1e6fb9a56d7e3ae868da41
EricG <ericrivas192@gmail.com>
2024-11-25 17:41:21 -0600

    cambios de produccion y archivos estaticos

--- PythonTest/settings/production.py
+++ PythonTest/settings/production.py
@@ -1,9 +1,13 @@
 from .base import *
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = 'RENDER' not in os.environ
 
 ALLOWED_HOSTS = []
+RENDER_EXTERNAL_HOSTNAME = os.environ.get('RENDER_EXTERNAL_HOSTNAME')
+
+if RENDER_EXTERNAL_HOSTNAME:
+    ALLOWED_HOSTS.append(RENDER_EXTERNAL_HOSTNAME)
 
 DATABASES = {
     'default': {
